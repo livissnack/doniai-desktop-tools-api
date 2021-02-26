6c95e075a38ed7929f22e08f28213968b62cea5d
guan xin <guanxin@tinytiger.cn>
2021-02-26 14:16:32 +0800

    新增虚拟币行情接口

--- app/Controllers/CoinController.php
+++ app/Controllers/CoinController.php
@@ -6,11 +6,26 @@ use ManaPHP\Rest\Controller;
 
 class CoinController extends Controller
 {
+    /**
+     * 虚拟币的当前价格
+     * @return array|string
+     */
     public function multiPriceAction()
     {
         $fsyms = input('fsyms', ['string', 'default' => 'BTC,ETH,XMR']);
         $tsyms = input('tsyms', ['string', 'default' => 'USD,CNY']);
         $url = env('CRYPTO_COMPARE_API_URL').'/data/pricemulti?'.http_build_query(['fsyms' => $fsyms, 'tsyms' => $tsyms]);
+        $key = env('CRYPTO_COMPARE_API_KEY');
+        return http_get($url, ['Authorization' => 'Apikey '.$key])->body;
+    }
+
+    /**
+     * 可交易的虚拟币
+     * @return array|string
+     */
+    public function availableAction()
+    {
+        $url = env('CRYPTO_COMPARE_API_URL').'/data/blockchain/list';
         $key = env('CRYPTO_COMPARE_API_KEY');
         return http_get($url, ['Authorization' => 'Apikey '.$key])->body;
     }
